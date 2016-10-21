c7067c83a309922a4d1ba927b3a5640b027d8b6c
ASUS-YUSHAOJUN\96853 <968538@163.com>
2016-10-21 22:37:42 +0800

    no message

--- application/controllers/Admin_login.php
+++ application/controllers/Admin_login.php
@@ -6,6 +6,15 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 */
 class Admin_login extends CI_Controller
 {
+	public function __construct()
+	{
+		parent::__construct();
+		if ( $this->session->userdata('user') == 'admin' ){
+			header('Location:'.base_url('admin'));
+		}
+		# code...
+	}
+
 	public function index()
 	{
 		//$this->session->set_userdata(['user'=>'admin']);
--- application/views/admin_login_view.php
+++ application/views/admin_login_view.php
@@ -42,21 +42,16 @@
                         <h3 class="panel-title">Please Sign In</h3>
                     </div>
                     <div class="panel-body">
-                        <form role="form">
+                        <form role="form" method="post" action="admin_login/login">
                             <fieldset>
                                 <div class="form-group">
-                                    <input class="form-control" placeholder="E-mail" name="email" type="email" autofocus>
+                                    <input class="form-control" placeholder="Username" name="username" type="text" autofocus>
                                 </div>
                                 <div class="form-group">
                                     <input class="form-control" placeholder="Password" name="password" type="password" value="">
                                 </div>
-                                <div class="checkbox">
-                                    <label>
-                                        <input name="remember" type="checkbox" value="Remember Me">Remember Me
-                                    </label>
-                                </div>
                                 <!-- Change this to a button or input when using this as a form -->
-                                <a href="index.html" class="btn btn-lg btn-success btn-block">Login</a>
+                                <input class="btn btn-lg btn-success btn-block" type="submit">
                             </fieldset>
                         </form>
                     </div>
