8102c12b8f0fc7e63c1c6e880a0b974ffd8274b0
hanrenguang <413526182@qq.com>
2016-10-22 21:07:46 +0800

    no message

---
+++ application/views/comment_list_view.php
@@ -0,0 +1,134 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>刺猬公社——内容产业第一报道媒体</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="<?php echo base_url();?>/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- MetisMenu CSS -->
+    <link href="<?php echo base_url();?>/vendor/metisMenu/metisMenu.min.css" rel="stylesheet">
+
+    <!-- DataTables CSS -->
+    <link href="<?php echo base_url();?>/vendor/datatables-plugins/dataTables.bootstrap.css" rel="stylesheet">
+
+    <!-- DataTables Responsive CSS -->
+    <link href="<?php echo base_url();?>/vendor/datatables-responsive/dataTables.responsive.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="<?php echo base_url();?>/dist/css/sb-admin-2.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="<?php echo base_url();?>/vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+
+    <link rel="stylesheet" href="<?php echo base_url();?>/dist/css/comment-list.css">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+
+    <?php $this->load->view('public_nav'); ?>
+        <div id="page-wrapper">
+            <!-- /.row -->
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="panel panel-default">
+                        <!-- /.panel-heading -->
+                        <div class="panel-body">
+                            <table width="100%" class="table table-striped table-bordered table-hover" id="dataTables-example">
+                                <thead>
+                                    <tr>
+                                        <th>文章标题 </th>
+                                        <th>昵称</th>
+                                        <th>评论内容</th>
+                                        <th>时间</th>
+                                        <th>操作</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈</td>
+                                        <td>呵呵呵呵呵呵呵呵呵呵呵呵呵呵呵</td>
+                                        <td>哗哗哗哗哗哗哗哗哗哗哗哗哗哗哗</td>
+                                        <td>吼吼吼吼吼吼吼吼吼吼吼吼吼吼吼</td>
+                                        <td><a href="javascript:;">通过</a><a href="javascript:;">拒绝</a></td>
+                                    </tr>
+                                    <tr>
+                                        <td>哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈</td>
+                                        <td>呵呵呵呵呵呵呵呵呵呵呵呵呵呵呵</td>
+                                        <td>哗哗哗哗哗哗哗哗哗哗哗哗哗哗哗</td>
+                                        <td>吼吼吼吼吼吼吼吼吼吼吼吼吼吼吼</td>
+                                        <td><a href="javascript:;">通过</a><a href="javascript:;">拒绝</a></td>
+                                    </tr>
+                                </tbody>
+                            </table>
+
+                            
+                              <ul id="pagination1" class="pagination">
+                                
+                              </ul>
+                            <!-- /.table-responsive -->
+                        </div>
+                        <!-- /.panel-body -->
+                    </div>
+                    <!-- /.panel -->
+                </div>
+                <!-- /.col-lg-12 -->
+            </div>
+        </div>
+        <!-- /#page-wrapper -->
+
+    </div>
+    <!-- /#wrapper -->
+
+    <!-- jQuery -->
+    <script src="<?php echo base_url();?>/vendor/jquery/jquery.min.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="<?php echo base_url();?>/vendor/bootstrap/js/bootstrap.min.js"></script>
+
+    <!-- Metis Menu Plugin JavaScript -->
+    <script src="<?php echo base_url();?>/vendor/metisMenu/metisMenu.min.js"></script>
+
+    <!-- DataTables JavaScript -->
+    
+
+    <!-- Custom Theme JavaScript -->
+    <script src="<?php echo base_url();?>/dist/js/sb-admin-2.js"></script>
+    <script src="<?php echo base_url();?>/dist/js/jqPaginator.min.js"></script>
+
+    <!-- Page-Level Demo Scripts - Tables - Use for reference -->
+    <script>
+    var count = true;
+    var currentPage = 1;
+    $('#pagination1').jqPaginator({
+        totalPages: 100,
+        visiblePages: 10,
+        currentPage: currentPage,
+        onPageChange: function (num, type) {
+            if(count) {
+                count = false;
+                return;
+            }
+        }
+    });
+    </script>
+
+</body>
+
+</html>
