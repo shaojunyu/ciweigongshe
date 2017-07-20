fe7a29540938a0e2b0f80b51d496420a2d99127e
Shaojun Yu <1024527135@qq.com>
2017-07-21 02:02:45 +0800

    api

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,55 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/admin', function () {
+    return View('admin.index');
+})->middleware('auth')->name('admin');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+
+//Author
+Route::get('admin/getAuthor','Author@getAuthor');
+Route::get('admin/getAuthorList','Author@getAuthorList');
+Route::get('admin/deleteAuthor','Author@deleteAuthor');
+Route::post('admin/addAuthor','Author@addAuthor');
+Route::post('admin/updateAuthor','Author@updateAuthor');
+
+//Tag
+Route::get('admin/getTagList');
+Route::get('admin/deleteTag');
+Route::post('admin/addTag');
+Route::post('admin/updateTag');
+
+//post
+Route::get('admin/getPost');
+Route::get('admin/getPostList');
+Route::get('admin/deletePost');
+Route::get('admin/publishPost');
+Route::get('admin/withdrawPost');
+Route::post('admin/updatePost');
+Route::post('admin/addPost');
+
+//news
+Route::get('admin/getNews');
+Route::get('admin/getNewsList');
+Route::get('admin/deleteNews');
+Route::get('admin/publishNews');
+Route::get('admin/withdrawNews');
+Route::post('admin/updateNews');
+Route::post('admin/addNews');
+
+//comment
+Route::get('admin/getCommentList');
+Route::get('admin/deleteComment');
+Route::post('admin/approveComment');
+Route::post('admin/rejectComment');
+
+//Resource
+Route::get('admin/getResourceList');
+Route::get('admin/deleteResource');
+Route::post('admin/addResource');
+Route::post('admin/updateResource');
